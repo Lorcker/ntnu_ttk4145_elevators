41a964f03c581a3b49e45c572700a9af3c7d49fa
Cevin Neubauer <cevin.neubauer@tum.de>
2025-02-25 10:44:52 +0100

    Moves config module into main to fix dep issues

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net"
+	"os"
 
 	"group48.ttk4145.ntnu/elevators/comms"
 	"group48.ttk4145.ntnu/elevators/elevatordriver"
@@ -80,4 +82,29 @@ func main() {
 		validatedRequests)
 
 	select {}
+}
+
+type Config struct {
+	ElevatorAddr string `json:"elevator_addr"`
+	NumFloors    int    `json:"num_floors"`
+	LocalPeerId  int    `json:"local_peer_id"`
+	LocalAddr    string `json:"local_addr"`
+	LocalPort    uint16 `json:"local_port"`
+}
+
+func LoadConfig(filename string) (*Config, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	config := &Config{}
+	err = decoder.Decode(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return config, nil
 }
