d53f2c497f573d8a551b812dca291f6604e94a19
Cevin Neubauer <cevin.neubauer@tum.de>
2025-03-17 12:23:15 +0100

    Cleans up healthmonitor module

--- internal/healthmonitor/monitor.go
+++ internal/healthmonitor/monitor.go
@@ -1,7 +1,12 @@
+// Description: This package is responsible for monitoring the health of the elevators.
+// It listens for pings from the elevators and tracks which elevators are alive.
+// It also informs the orders and requests modules about the alive elevators.
+// The RunMonitor function runs the health monitor.
 package healthmonitor
 
 import (
 	"log"
+	"reflect"
 	"time"
 
 	"group48.ttk4145.ntnu/elevators/internal/models"
@@ -13,10 +18,12 @@ const Timeout = time.Second * 10
 // PollInterval is the frequency at which the monitor informs about alive elevators.
 const PollInterval = time.Second * 1
 
+// lastSeen is a map of the last time a ping was received from an elevator.
 type lastSeen = map[models.Id]time.Time
 
-// RunMonitor runs the health monitor. It listens for pings from the elevators
-// and tracks which elevators are alive.
+// RunMonitor runs the health monitor
+//
+// It listens for pings from the elevators and tracks which elevators are alive.
 func RunMonitor(
 	local models.Id,
 	pingFromComms <-chan models.Id,
@@ -27,7 +34,6 @@ func RunMonitor(
 	var alivePeers = make(map[models.Id]bool)
 
 	ticker := time.NewTicker(PollInterval)
-	defer ticker.Stop()
 
 	for {
 		select {
@@ -38,12 +44,15 @@ func RunMonitor(
 			lastSeen[id] = time.Now()
 		case <-ticker.C:
 			a := getAlive(lastSeen)
-			a[local] = true
-			if !isDifferent(a, alivePeers) {
+			a[local] = true // Always consider yourself alive
+
+			if reflect.DeepEqual(a, alivePeers) {
+				// No need to notify if the list of alive peers is the same
 				continue
 			}
 
 			alivePeers = a
+
 			log.Printf("[healthmonitor] Notifying [orders] and [requests] that he alive list changed: %v", a)
 			s := mapToSlice(alivePeers)
 			alivenessToOrders <- s
@@ -52,6 +61,9 @@ func RunMonitor(
 	}
 }
 
+// getAlive returns a map of the alive elevators
+//
+// An elevator is considered alive if a ping has been received from it within the last Timeout.
 func getAlive(ls lastSeen) map[models.Id]bool {
 	var a = make(map[models.Id]bool)
 	for id, t := range ls {
@@ -62,18 +74,7 @@ func getAlive(ls lastSeen) map[models.Id]bool {
 	return a
 }
 
-func isDifferent(a, b map[models.Id]bool) bool {
-	if len(a) != len(b) {
-		return true
-	}
-	for id := range a {
-		if _, ok := b[id]; !ok {
-			return true
-		}
-	}
-	return false
-}
-
+// mapToSlice converts a map to a slice
 func mapToSlice(m map[models.Id]bool) []models.Id {
 	s := make([]models.Id, 0, len(m))
 	for id := range m {
